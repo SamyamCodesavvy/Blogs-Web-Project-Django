35de6a0f4305458446a92449185437629c627a98
SamyamCodesavvy <samyam0913@gmail.com>
2024-06-03 16:07:54 +0545

    blueprint of views.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from . import views
 urlspattern = [
-    path(""),
-    path("posts"),
-    path("posts/<slug>") #posts/my-first-post
+    path("", views.starting_page),
+    path("posts", views.posts),
+    path("posts/<slug: slug>", views.post_detail) #posts/my-first-post
 ]
