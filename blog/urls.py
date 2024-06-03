929907e5418a1a48831dc368db652932b0f9eab7
SamyamCodesavvy <samyam0913@gmail.com>
2024-06-03 16:23:59 +0545

    created templates and base.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlspattern = [
-    path("", views.starting_page),
-    path("posts", views.posts),
-    path("posts/<slug: slug>", views.post_detail) #posts/my-first-post
-]
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug: slug>", views.post_detail, name="post") #posts/my-first-post
+] 
